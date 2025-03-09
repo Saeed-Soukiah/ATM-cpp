56213c160b23f27960aa170300bcf69d8af54206
Saeed-Soukiah <saeedsoukiah@gmail.com>
2025-03-10 00:22:01 +0300

    ATM c++.cpp

--- ATM c++.cpp
+++ ATM c++.cpp
@@ -1,12 +1,6 @@
 /* 
 Author: Saeed Soukiah 
 Date: December 8, 2024 
-Description: This program simulates an ATM machine. It allows users to: 
-- Enter their name and password. - Check their account balance. 
-- Make a withdrawal, provided they have sufficient funds. 
-- Deposit money into their account. 
-- Validate a credit card number to ensure it is legitimate. 
-The user interaction is handled via a simple menu system, and the program includes basic error handling to prompt the user for correct input when necessary. 
 */
 #include <iostream>
 #include <string>
